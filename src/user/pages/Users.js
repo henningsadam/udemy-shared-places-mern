4d75edd435c762558684e244b1f997269c217e5c
Adam Hennings <hennings.adam@gmail.com>
2024-03-12 16:39:33 -0600

    created the UsersList, UserItem, Avatar, and Card components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+  const USERS_MOCKED_DATA = [
+    {
+      id: 'u1',
+      name: 'Sofia Uza',
+      image:
+        'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS_MOCKED_DATA} />;
+};
 
-export default Users
+export default Users;
