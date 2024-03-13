33cfe28cb638e6e66a5143b03659bc01c14b13e4
Adam Hennings <hennings.adam@gmail.com>
2024-03-13 11:36:12 -0600

    added google maps embed

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
 import './PlaceItem.css';
 
 const PlaceItem = (props) => {
@@ -23,7 +24,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={toggleShowMap}>Close</Button>}
       >
         <div className='map-container'>
-          <h2>The map!</h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className='place-item'>
@@ -37,7 +38,9 @@ const PlaceItem = (props) => {
             <p>{props.description}</p>
           </div>
           <div className='place-item__actions'>
-            <Button inverse onClick={toggleShowMap}>View</Button>
+            <Button inverse onClick={toggleShowMap}>
+              View
+            </Button>
             <Button to={`/places/${props.id}`}>Edit</Button>
             <Button danger>Delete</Button>
           </div>
---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,26 @@
+import React, { useRef, useEffect } from 'react';
+
+import './Map.css';
+
+const Map = (props) => {
+  const mapRef = useRef();
+  // const { center, zoom } = props;
+
+  // useEffect(() => {
+  //   const map = new window.google.maps.Map(mapRef.current, {
+  //     center: center,
+  //     zoom: zoom,
+  //   });
+
+  //   new window.google.maps.Marker({ position: center, map: map });
+  // }, [center, zoom]);
+
+  return (
+    <div ref={mapRef} className={`map ${props.className}`} style={props.style}>
+      <strong style={{'fontSize': '1.5rem'}}>Google Maps Embed</strong>
+      <p>(excluded to conceal API key in front-end)</p>
+    </div>
+  );
+};
+
+export default Map;
